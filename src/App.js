f283d14bdbdd8c67bf75efb0b1ccd9b5cc7ffb58
Chaten Mundle <chetanmundle7875@gmail.com>
2024-03-15 13:27:40 +0530

    chetan first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
-import './responsive.css'
-import Carousel from './components/Carousel';
-import AboutSection from './components/AboutSection';
-import Services from './components/Services';
-import SocialPage from './components/SocialPage';
-import Navbar from './components/Navbar';
-import LatestNews from './components/LatestNews';
-import GalleryComponent from './components/GalleryComponent';
-import Temples from './components/Temples';
-import NearbyPlaces from './components/NearbyPlaces';
-import Preloader from './components/Preloader';
-import Footer from './components/Footer';
-import { useState, useEffect } from 'react';
+import "./App.css";
+import "./responsive.css";
+import Carousel from "./components/Carousel";
+import AboutSection from "./components/AboutSection";
+import Services from "./components/Services";
+import SocialPage from "./components/SocialPage";
+import Navbar from "./components/Navbar";
+import LatestNews from "./components/LatestNews";
+import GalleryComponent from "./components/GalleryComponent";
+import Temples from "./components/Temples";
+import NearbyPlaces from "./components/NearbyPlaces";
+import Preloader from "./components/Preloader";
+import Footer from "./components/Footer";
+import { useState, useEffect } from "react";
 
 function App() {
-   
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -26,21 +25,23 @@ function App() {
 
   return (
     <div className="App">
-
-      {loading ? <Preloader /> : 
-      
-      
-
-    <><Navbar />
-      <SocialPage />
-      <Carousel />
-      <AboutSection />
-       <Services /> 
-       <LatestNews /> 
-       <GalleryComponent /> 
-      <Temples />
-      <NearbyPlaces />
-      <Footer /></>  }
+      {loading ? (
+        <Preloader />
+      ) : (
+        <>
+        
+          <Navbar />
+          <SocialPage />
+          <Carousel />
+          <AboutSection />
+          <Services />
+          <LatestNews />
+          <GalleryComponent />
+          <Temples />
+          <NearbyPlaces />
+          <Footer />
+        </>
+      )}
     </div>
   );
 }
--- src/components/Temples.jsx
+++ src/components/Temples.jsx
@@ -1,112 +1,93 @@
-import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import {Autoplay,  Pagination } from 'swiper/modules';
-import Gurusthan from '../assets/images/Gurusthan.png'
-import Samadhi from '../assets/images/Samadhi.jpg'
-import Dwarkamai from '../assets/images/dwarka.jpg'
-import Chavadi from '../assets/images/chavadi.jpg'
-import Lendi from '../assets/images/lendibaug.jpg'
-import Other from '../assets/images/other-temples.jpg'
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+import Gurusthan from "../assets/images/Gurusthan.png";
+import Samadhi from "../assets/images/Samadhi.jpg";
+import Dwarkamai from "../assets/images/dwarka.jpg";
+import Chavadi from "../assets/images/chavadi.jpg";
+import Lendi from "../assets/images/lendibaug.jpg";
+import Other from "../assets/images/other-temples.jpg";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/virtual';
-import 'swiper/css/pagination';
-
-
+import "swiper/css";
+import "swiper/css/virtual";
+import "swiper/css/pagination";
 
 const Temples = () => {
+  return (
+    <section id="swipe">
+      <div className="title ">
+        <h1>
+          <span className="temple-premise-span">Temple in Premise</span>
+        </h1>
+      </div>
+      <Swiper
+        slidesPerView={"auto"}
+        loop={true}
+        autoplay={{
+          delay: 2000, // Delay between slides in milliseconds (5 seconds in this case)
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          355: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+        }}
+        modules={[Autoplay, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="slide card">
+            <img src={Gurusthan} alt="" />
+            <h2>Gurusthan</h2>
+          </div>
+        </SwiperSlide>
 
+        <SwiperSlide>
+          <div className="slide">
+            <img src={Samadhi} alt="" />
+            <h2>Samadhi</h2>
+          </div>
+        </SwiperSlide>
 
+        <SwiperSlide>
+          <div className="slide">
+            <img src={Dwarkamai} alt="" />
+            <h2>Dwarkamai</h2>
+          </div>
+        </SwiperSlide>
 
-    return (
-        <section id="swipe">
-            
-              <div className="title twelve">
-                <h1>Temple in Premise</h1>
-              </div>
-          <Swiper
-                
-                slidesPerView={"auto"}
-                loop={true}
-                autoplay = {{
-                    delay: 2000, // Delay between slides in milliseconds (5 seconds in this case)
-                    disableOnInteraction: false,
-                }}
-                pagination={{
-                    clickable: true,
-                }}
+        <SwiperSlide>
+          <div className="slide">
+            <img src={Chavadi} alt="" />
+            <h2>Chavadi</h2>
+          </div>
+        </SwiperSlide>
 
-                breakpoints={{
-                    355: {
-                      slidesPerView: 1,
-                      spaceBetween: 20,
-                    },
-                    768: {
-                      slidesPerView: 4,
-                      spaceBetween: 40,
-                    }}}
-                
-                modules={[Autoplay,Pagination]}
-                className="mySwiper"
-            >
+        <SwiperSlide>
+          <div className="slide">
+            <img src={Lendi} alt="" />
+            <h2>Lendi Garden</h2>
+          </div>
+        </SwiperSlide>
 
+        <SwiperSlide>
+          <div className="slide">
+            <img src={Other} alt="" />
+            <h2>Other Temples</h2>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  );
+};
 
-
-                <SwiperSlide >
-                    <div className='slide card'>
-                        <img src={Gurusthan} alt=""  />
-                        <h2>Gurusthan</h2>
-                        
-                    </div>
-                </SwiperSlide>
-
-                <SwiperSlide>
-                    <div className='slide'>
-                        <img src={Samadhi} alt="" />
-                        <h2>Samadhi</h2>
-                    </div>
-                </SwiperSlide>
-
-                <SwiperSlide>
-                    <div className='slide'>
-                        <img src={Dwarkamai} alt="" />
-                        <h2>Dwarkamai</h2>
-                    </div>
-                </SwiperSlide>
-
-
-                <SwiperSlide>
-                    <div className='slide'>
-                        <img src={Chavadi} alt="" />
-                        <h2>Chavadi</h2>
-                    </div>
-                </SwiperSlide>
-
-
-                <SwiperSlide>
-                    <div className='slide'>
-                        <img src={Lendi} alt="" />
-                        <h2>Lendi Garden</h2>
-                    </div>
-                </SwiperSlide>
-
-
-                <SwiperSlide>
-                    <div className='slide'>
-                        <img src={Other} alt="" />
-                        <h2>Other  Temples</h2>
-                    </div>
-                </SwiperSlide>
-
-
-                
-
-             </Swiper>
-         
-
-        </section>
-    )
-}
-
-export default Temples
+export default Temples;
