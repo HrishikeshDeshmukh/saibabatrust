b9de964c27c71f893b7923321ed8abdd022a2d6e
Chaten Mundle <chetanmundle7875@gmail.com>
2024-03-19 17:28:44 +0530

    19.03.24 5.28pm

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import "./App.css";
 import "./responsive.css";
+import "./responsive1.css";
 import "./App1.css";
-import Carousel from "./components/Carousel";
-import AboutSection from "./components/AboutSection";
-import Services from "./components/Services";
-import SocialPage from "./components/SocialPage";
-import Navbar from "./components/Navbar";
-import LatestNews from "./components/LatestNews";
-import GalleryComponent from "./components/GalleryComponent";
-import Temples from "./components/Temples";
-import NearbyPlaces from "./components/NearbyPlaces";
-import Preloader from "./components/Preloader";
-import Footer from "./components/Footer";
-import { useState, useEffect } from "react";
 import Aos from "aos";
-import DailyUpdate from "./components/Update";
 import { Route, Router, Routes } from "react-router-dom";
-// import Home from "./pages/Home";
 import Home from "./pages/Home";
 
 function App() {
--- src/components/SocialPage.jsx
+++ src/components/SocialPage.jsx
@@ -1,42 +1,39 @@
-import React from 'react'
-import { FaFacebook,FaInstagram, FaTwitter, FaYoutube} from "react-icons/fa";
-import Donate from '../assets/images/donate.png'
-
+import React from "react";
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+import Donate from "../assets/images/donate.png";
 
 const SocialPage = () => {
   return (
     <section>
-
-      <div className="social-links adjust"> 
-      <div className="social-btn reds" >
-        <img src={Donate} alt=""  /><span>Donate</span>
-
+      <div className="social-links adjust">
+        <div className="social-btn reds">
+          <img src={Donate} alt="" />
+          <span>Donate</span>
         </div>
-</div>
+      </div>
       <div className="social-links">
-        <div className="social-btn" >
-        <FaFacebook id='facebook'/><span>@shrisaibabasansthantrustshirdi</span>
-
+        <div className="social-btn">
+          <FaFacebook id="facebook" />
+          <span>@shrisaibabasansthantrustshirdi</span>
         </div>
 
         <div className="social-btn">
-        <FaInstagram id='insta'/><span>@shreesaibabasansthantrust</span>
-
+          <FaInstagram id="insta" />
+          <span>@shreesaibabasansthantrust</span>
         </div>
 
         <div className="social-btn">
-        <FaTwitter id='twitter'/><span>@ssstshirdi</span>
-
+          <FaTwitter id="twitter" />
+          <span>@ssstshirdi</span>
         </div>
 
         <div className="social-btn">
-        <FaYoutube id='youtube'/><span>@saibabasanstantrust</span>
-
+          <FaYoutube id="youtube" />
+          <span>@saibabasanstantrust</span>
         </div>
-
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default SocialPage
+export default SocialPage;
